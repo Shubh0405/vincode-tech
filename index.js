35e3464b41a317f3841b8f22d8b7035cd5d36553
kvssankar <kvs.sankar001@gmail.com>
2020-11-15 14:40:14 +0530

    heroku deployed

---
+++ index.js
@@ -0,0 +1,89 @@
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bp=require("body-parser");
+const app = express();
+
+app.use(bp.json());
+app.use(bp.urlencoded({
+    extended: true
+  }));
+
+app.use(express.static(__dirname + '/public'));
+
+const db = "mongodb+srv://kvssankar:sankarvishnu23@cluster1.uacfw.mongodb.net/vinprep?retryWrites=true&w=majority";
+
+
+var contactSchema=new mongoose.Schema({
+    name:String,
+    email:String,
+    number:String,
+    subject:String,
+    matter:String
+})
+
+var contact = mongoose.model("contact", contactSchema);
+
+//connect to mongo
+const connect = mongoose
+  .connect(db, { useFindAndModify: false })
+  .then(() => console.log("Mondo db connected...."))
+  .catch((err) => console.log(err));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.get("/about", function (req, res) {
+    res.sendFile(__dirname+"/about.html");
+});
+
+app.get("/faq", function (req, res) {
+    res.sendFile(__dirname+"/faq.html");
+});
+
+app.get("/contact", function (req, res) {
+    res.sendFile(__dirname+"/contact.html");
+});
+
+// app.post("/contactus",function(req,res){
+//     contact.create({
+//         name: req.body.name,
+//         phone: req.body.phone,
+//         email: req.body.eaddress,
+//         subject:req.body.subject,
+//         matter: req.body.matter
+//     },
+//     function (err, yolo) {
+//         if (err) {
+//             console.log("DATA IS NOT PUSHED");
+//         } else {
+//             console.log("DATA HAS BEEN PUSHED");
+//             res.sendFile(__dirname+"//");
+//         }
+//     }
+//     )
+// });
+
+// app.post("/riskpredictor",function(req,res){
+//     risk.create({
+//         name: req.body.name,
+//         email: req.body.eaddress,
+//         score:req.body.score,
+//         matter: req.body.matter
+//     },
+//     function (err, yolo) {
+//         if (err) {
+//             console.log("DATA IS NOT PUSHED");
+//         } else {
+//             console.log("DATA HAS BEEN PUSHED");
+//             res.json({"score":req.body.score});
+//         }
+//     }
+//     )
+// });
+
+
+app.listen(process.env.PORT || 1111, function () {
+    console.log("SERVER 8000 HAS STARTED");
+});
